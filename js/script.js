33efbffe0fec98590860258067259b8ddbce816c
Victor Abrell <victor@abrell.se>
2019-11-13 08:50:04 +0100

    Added sections and show/hide functionality

--- js/script.js
+++ js/script.js
@@ -22,13 +22,27 @@ addEventListener('load', () => {
   const tabs = document.querySelectorAll('.tabs li')
 
   if ( tabs.length > 0 ) {
+
+    document.querySelectorAll('section').forEach(section => {
+      if(document.querySelector('.is-active').dataset.target !== section.id) {
+        section.style.display = 'none'
+      }
+    })
+
     
     tabs.forEach( tab => {
       
       tab.addEventListener('click', () => {
         if( !tab.classList.contains('is-active') ) {
+
+          const dataTarget = tab.dataset.target
           
           document.querySelector('.is-active').classList.remove('is-active')
+          document.querySelectorAll('section').forEach(section => {
+            section.style.display = 'none'
+          })
+
+          document.querySelector(`#${dataTarget}`).style.display = 'block'
           tab.classList.add('is-active')
         }
       })
