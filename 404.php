87d8747d2802105bbbd5eb18b90ca1ee2c6d3f61
Victor Abrell <victor@abrell.se>
2019-11-14 18:37:23 +0100

    Added 404 page

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+get_header();
+?>
+
+<section class="section has-text-centered">
+  <h1 class="title is-2">(╥﹏╥)</h1>
+  <h2 class="subtitle is-4">
+    <?php
+    echo __("I can't find the page...")
+    ?>
+    </h2>
+</section>
+
+<?php
+get_footer();
