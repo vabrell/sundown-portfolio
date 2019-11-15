b495a69414c689378199fee698e7eb2cfca6d6db
Victor Abrell <victor@abrell.se>
2019-11-15 18:30:42 +0100

    Updated 404 message with cake

--- 404.php
+++ 404.php
@@ -3,10 +3,14 @@ get_header();
 ?>
 
 <section class="section has-text-centered">
-  <h1 class="title is-2">(╥﹏╥)</h1>
+  <h1 class="title is-1">
+    <span class="icon">
+      <i class="mdi mdi-cake"></i>
+    </span>
+  </h1>
   <h2 class="subtitle is-4">
     <?php
-    echo __("I can't find the page...")
+    echo __("Can't find the page... but have some cake instead!")
     ?>
     </h2>
 </section>
